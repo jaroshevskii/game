7c5e5756fed18eb91d7e2e21e24e40a94b3d4999
jaroshevski <jaroshevskiisasha@gmail.com>
2021-12-24 17:15:43 +0200

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,30 +10,36 @@ struct Window {
 };
 
 int main() {
-  Window window{840, 360, "Game"};
+  Window window{640, 270, "Game"};
 
   InitWindow(window.width, window.height, window.title);
   SetTargetFPS(60);
 
-  Vector2 ballPosition{window.width / 2.0f, window.height / 2.0f};
+  Vector2 position{304.0f, 119.0f};
 
-  float speed = 10.0f;
+  float speed;
 
   while (!WindowShouldClose()) {
+    speed = 5.0f;
+
+    std::cout << "x: " << position.x << " y: " << position.y << '\n';
+
+    if (IsKeyDown(KEY_LEFT_SHIFT))
+      speed *= 2.0f;
     if (IsKeyDown(KEY_RIGHT))
-      ballPosition.x += speed;
+      position.x += speed;
     if (IsKeyDown(KEY_LEFT))
-      ballPosition.x -= speed;
+      position.x -= speed;
     if (IsKeyDown(KEY_UP))
-      ballPosition.y -= speed;
+      position.y -= speed;
     if (IsKeyDown(KEY_DOWN))
-      ballPosition.y += speed;
+      position.y += speed;
 
     BeginDrawing();
-
     ClearBackground(BLANK);
-    DrawCircleV(ballPosition, 50, MAROON);
-    DrawRectangleV({ballPosition.x - 40, ballPosition.y - 10}, {80, 20}, WHITE);
+
+    //DrawCircleV(ballPosition, 50, MAROON);
+    DrawRectangleV({position.x, position.y}, {32, 32}, MAGENTA);
 
     DrawFPS(10, 10);
     EndDrawing();
